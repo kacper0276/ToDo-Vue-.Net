d4a68a96686798014cf427f41c183a3f65890f3e
kacper0276 <kacper0276@op.pl>
2024-08-18 00:38:36 +0200

    [FE] Create method to set title page

---
+++ frontend/src/composables/setDocumentTitle.ts
@@ -0,0 +1,30 @@
+import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
+
+export function setDocumentTitle(key: string) {
+  const { t, locale } = useI18n();
+  const title = ref(t(key));
+
+  const setTitle = (newKey: string) => {
+    title.value = t(newKey);
+    document.title = title.value;
+  };
+
+  watch(
+    title,
+    (newTitle) => {
+      setTitle(newTitle);
+    },
+    { immediate: true }
+  );
+
+  watch(
+    [locale, () => t(key)],
+    () => {
+      setTitle(key);
+    },
+    { immediate: true }
+  );
+
+  return { title, setTitle };
+}
