942ad26cd245d41f2f6a28387277eb69e06f3fd7
kacper0276 <kacper0276@op.pl>
2024-08-18 01:02:52 +0200

    [FE] Use ToDoItem component in HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
+import ToDoItem from "@/components/ToDoItem.vue";
 import { setDocumentTitle } from "../composables/setDocumentTitle";
+import { ref } from "vue";
 
 const { setTitle } = setDocumentTitle("home-page");
 
 setTitle("home-page");
+
+const toDoItem = ref({
+  id: 1,
+  title: "Przykładowe zadanie",
+  description: "To jest przykładowy opis zadania.",
+  completed: false,
+});
 </script>
 
 <template>
-  <main>
-    <p>Strona główna</p>
+  <main class="main-container">
+    <ToDoItem :to-do-item="toDoItem" />
+    <ToDoItem :to-do-item="toDoItem" />
+    <ToDoItem :to-do-item="toDoItem" />
+    <ToDoItem :to-do-item="toDoItem" />
+    <ToDoItem :to-do-item="toDoItem" />
+    <ToDoItem :to-do-item="toDoItem" />
   </main>
 </template>
+
+<style scoped>
+.main-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
