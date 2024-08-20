576210b5912ce1564e9b4841ed6df9c6f285c692
kacper0276 <kacper0276@op.pl>
2024-08-20 03:37:35 +0200

    [FE] Create method for delete todos

--- frontend/src/composables/useToDo.ts
+++ frontend/src/composables/useToDo.ts
@@ -1,12 +1,13 @@
 import todoService from "@/services/todoService";
 import type { ToDoItem } from "@/types/ToDoItem.type";
-import { ref, type Ref } from "vue";
+import { inject, ref, type Ref } from "vue";
 
 export function useToDo() {
   const todos: Ref<ToDoItem[]> = ref([]);
   const todo: Ref<ToDoItem | null> = ref(null);
-  const loading: Ref<boolean> = ref(false);
   const error: Ref<string | null> = ref(null);
+
+  const loading = inject("isLoading", ref(false));
 
   const fetchToDoItems = async (): Promise<void> => {
     loading.value = true;
@@ -28,7 +29,6 @@ export function useToDo() {
     error.value = null;
     try {
       await todoService.addNewToDo(item);
-      await fetchToDoItems();
     } catch (err) {
       error.value = err instanceof Error ? err.message : String(err);
     } finally {
@@ -41,7 +41,18 @@ export function useToDo() {
     error.value = null;
     try {
       await todoService.toggleToDoStatus(id);
-      await fetchToDoItems();
+    } catch (err) {
+      error.value = err instanceof Error ? err.message : String(err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const deleteToDoItem = async (id: number): Promise<void> => {
+    loading.value = true;
+    error.value = null;
+    try {
+      await todoService.deleteToDoItem(id);
     } catch (err) {
       error.value = err instanceof Error ? err.message : String(err);
     } finally {
@@ -57,5 +68,6 @@ export function useToDo() {
     fetchToDoItems,
     addToDoItem,
     toggleToDoStatus,
+    deleteToDoItem,
   };
 }
