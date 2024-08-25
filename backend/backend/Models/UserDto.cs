4f7aafde4aee3718413c65d17ed8452bf51bdc47
kacper0276 <kacper0276@op.pl>
2024-08-25 02:27:11 +0200

    [BE] Create UserDto

---
+++ backend/backend/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class UserDto
+    {
+        public string? Email { get; set; }
+        public string? Login { get; set; }
+        public string? Role { get; set; }
+    }
+}
