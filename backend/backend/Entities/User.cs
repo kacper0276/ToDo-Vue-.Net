a42912b084f0504d02d34b61f7bf596b81b81550
kacper0276 <kacper0276@op.pl>
2024-08-25 02:26:28 +0200

    [BE] Update structure in user.cs

--- backend/backend/Entities/User.cs
+++ backend/backend/Entities/User.cs
@@ -3,9 +3,9 @@
     public class User
     {
         public int Id { get; set; }
-        public string? Login { get; set; }
-        public string? Password { get; set; }
-        public string? Email { get; set; }
+        public required string Login { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
         public string? Role { get; set; }
     }
 }
