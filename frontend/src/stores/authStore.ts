798b1f42bd2e916e4ecc6691d23c31ed442c76b5
kacper0276 <kacper0276@op.pl>
2024-08-27 01:48:52 +0200

    [FE] Create authStore (Pinia)

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,19 +1,33 @@
 import { defineStore } from "pinia";
 import type { User } from "@/types/Auth.type";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("auth", () => {
   const user = ref<User | null>(null);
   const loggedIn = ref(false);
+  const router = useRouter();
 
   const setUser = (userData: User | null) => {
     user.value = userData;
     loggedIn.value = !!userData;
   };
 
+  const logout = () => {
+    user.value = null;
+    loggedIn.value = false;
+
+    console.log("Test");
+
+    sessionStorage.removeItem("authToken");
+    sessionStorage.removeItem("refreshToken");
+    router.push("/login");
+  };
+
   return {
     user,
     loggedIn,
     setUser,
+    logout,
   };
 });
