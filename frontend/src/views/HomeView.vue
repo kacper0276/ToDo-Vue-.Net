a7a2b988799f556c4fb4d75ab8aaa7cdda67c45e
kacper0276 <kacper0276@op.pl>
2024-08-28 00:35:48 +0200

    [FE] Change in home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,51 +1,16 @@
 <script setup lang="ts">
-import ToDoItemComponent from "@/components/ToDoItem.vue";
-import AddTodoModal from "@/components/AddTodoModal.vue";
-import { onMounted, ref } from "vue";
 import { setDocumentTitle } from "@/composables/setDocumentTitle";
-import { useToDo } from "@/composables/useToDo";
-import { useNotification } from "@kyvg/vue3-notification";
 import { useI18n } from "vue-i18n";
 
-const notification = useNotification();
 const { setTitle } = setDocumentTitle("home-page");
 setTitle("home-page");
 
 const { t } = useI18n();
-
-const { fetchToDoItems, todos } = useToDo();
-
-onMounted(async () => {
-  await fetchToDoItems();
-});
-
-const isModalVisible = ref(false);
-
-const showAddTodoModal = () => {
-  isModalVisible.value = true;
-};
-
-const hideAddTodoModal = () => {
-  isModalVisible.value = false;
-};
 </script>
 
 <template>
   <main class="main-container">
-    <button @click="showAddTodoModal" class="add-button">
-      {{ t("add-new-task") }}
-    </button>
-    <ToDoItemComponent
-      v-for="item in todos"
-      :key="item.id"
-      :to-do-item="item"
-      :on-refresh="fetchToDoItems"
-    />
-    <AddTodoModal
-      :show="isModalVisible"
-      :onClose="hideAddTodoModal"
-      :on-refresh="fetchToDoItems"
-    />
+    <h1>Witaj na stronie głównej</h1>
   </main>
 </template>
 
@@ -56,21 +21,5 @@ const hideAddTodoModal = () => {
   flex-wrap: wrap;
   justify-content: center;
   padding: 20px;
-}
-
-.add-button {
-  background-color: #42b97c;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  padding: 10px 20px;
-  font-size: 1em;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  margin-bottom: 20px;
-}
-
-.add-button:hover {
-  background-color: #36a56d;
 }
 </style>
