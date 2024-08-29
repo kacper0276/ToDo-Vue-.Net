96317bfc1a31ff6927a4bcfb77b62f011e9999be
kacper0276 <kacper0276@op.pl>
2024-08-29 06:11:16 +0200

    [FE] Create paths for user and admin panel

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,6 +4,8 @@ import LoginView from "@/views/LoginView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import ToDoView from "@/views/ToDoView.vue";
+import AdminPanelView from "@/views/AdminPanelView.vue";
+import UserPanelView from "@/views/UserPanelView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +29,16 @@ const router = createRouter({
       path: "/to-dos",
       name: "to-dos",
       component: ToDoView,
+    },
+    {
+      path: "/admin-panel",
+      name: "admin-panel",
+      component: AdminPanelView,
+    },
+    {
+      path: "/user-panel",
+      name: "user-panel",
+      component: UserPanelView,
     },
     {
       path: "/:pathMatch(.*)*",
