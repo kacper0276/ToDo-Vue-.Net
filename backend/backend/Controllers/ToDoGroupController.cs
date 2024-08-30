1676068be8666cc0c4e0c8a105db2c0c2e14506f
kacper0276 <kacper0276@op.pl>
2024-08-31 00:03:33 +0200

    [BE] Create ToDoGroupController

---
+++ backend/backend/Controllers/ToDoGroupController.cs
@@ -0,0 +1,26 @@
+﻿using backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/todogroup")]
+    [ApiController]
+    public class ToDoGroupController : ControllerBase
+    {
+        private readonly IToDoGroupService _service;
+
+        public ToDoGroupController(IToDoGroupService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> GetAll([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            var result = await _service.GetAllAsync(pageNumber, pageSize);
+            return Ok(result);
+        }
+    }
+}
