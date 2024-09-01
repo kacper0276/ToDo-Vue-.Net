166d333ac4b33258b961cff5a5d4db60a66f2bf8
kacper0276 <kacper0276@op.pl>
2024-09-01 09:42:27 +0200

    [FE] Create ToDoItem type

--- frontend/src/types/ToDoItem.type.ts
+++ frontend/src/types/ToDoItem.type.ts
@@ -1,8 +1,12 @@
+import type { ToDoGroup } from "./ToDoGroup.type";
+
 export class ToDoItem {
   constructor(
     public id: number,
     public title: string,
     public description: string,
-    public isComplete: boolean
+    public isComplete: boolean,
+    public toDoGroupId: number,
+    public toDoGroup?: ToDoGroup
   ) {}
 }
