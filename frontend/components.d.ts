d41d67f8b9d2948df11a853b034b8cac179c8395
kacper0276 <kacper0276@op.pl>
2024-09-05 00:05:02 +0200

    [FE] Changes in components declaration imports

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -4,12 +4,13 @@ export {};
 
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
-    ToDoItem: typeof import("./src/components/ToDoItem.vue")["default"];
-    AddTodoModal: typeof import("./src/components/AddTodoModal.vue")["default"];
-    LoadingSpinner: typeof import("./src/components/LoadingSpinner.vue")["default"];
-    LoginForm: typeof import("./src/components/LoginForm.vue")["default"];
-    RegisterForm: typeof import("./src/components/RegisterForm.vue")["default"];
-    ChatComponent: typeof import("./src/components/ChatComponent.vue")["default"];
-    UserToDoGroup: typeof import("./src/components/UserToDoGroup.vue")["default"];
+    ToDoItem: typeof import("./src/components/todo/ToDoItem.vue")["default"];
+    AddTodoModal: typeof import("./src/components/modals/AddTodoModal.vue")["default"];
+    LoadingSpinner: typeof import("./src/components/ui/LoadingSpinner.vue")["default"];
+    LoginForm: typeof import("./src/components/forms/LoginForm.vue")["default"];
+    RegisterForm: typeof import("./src/components/forms/RegisterForm.vue")["default"];
+    ChatComponent: typeof import("./src/components/chat/ChatComponent.vue")["default"];
+    UserToDoGroup: typeof import("./src/components/todo/UserToDoGroup.vue")["default"];
+    AddNewToDosGroupForm: typeof import("./src/components/forms/AddNewToDosGroupForm.vue")["default"];
   }
 }
