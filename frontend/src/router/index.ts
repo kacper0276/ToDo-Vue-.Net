4902058c2648546eabe02967dc4f44f58479a59f
kacper0276 <kacper0276@op.pl>
2024-09-05 00:05:17 +0200

    [FE] Changes in routing imports

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import LoginView from "@/views/LoginView.vue";
-import NotFoundView from "@/views/NotFoundView.vue";
-import RegisterView from "@/views/RegisterView.vue";
-import ToDoView from "@/views/ToDoView.vue";
-import AdminPanelView from "@/views/AdminPanelView.vue";
-import UserPanelView from "@/views/UserPanelView.vue";
+import HomeView from "@/views/home/HomeView.vue";
+import LoginView from "@/views/auth/LoginView.vue";
+import NotFoundView from "@/views/home/NotFoundView.vue";
+import RegisterView from "@/views/auth/RegisterView.vue";
+import ToDoView from "@/views/todo/ToDoView.vue";
+import AdminPanelView from "@/views/admin/AdminPanelView.vue";
+import UserPanelView from "@/views/user/UserPanelView.vue";
 import { authGuard } from "@/guards/authGuard";
 import { adminGuard } from "@/guards/adminGuard";
-import AddNewToDosGroupView from "@/views/AddNewToDosGroupView.vue";
+import AddNewToDosGroupView from "@/views/todo/AddNewToDosGroupView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
