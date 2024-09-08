f98ebf1108f51be79378089f6b746e95f3b5599d
kacper0276 <kacper0276@op.pl>
2024-09-08 10:24:55 +0200

    [FE] Create index in types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./auth/Auth.type";
+export * from "./server/ServerResponseList.type";
+export * from "./server/ServerResponsePaginated.type";
+export * from "./server/ServerResponseSimple.type";
+export * from "./todo/ToDoGroup.type";
+export * from "./todo/ToDoItem.type";
