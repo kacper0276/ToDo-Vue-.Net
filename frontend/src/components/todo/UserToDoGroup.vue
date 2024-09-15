ebcf3f879990e1caa968e20bb60a844a08b3a766
kacper0276 <kacper0276@op.pl>
2024-09-16 00:02:48 +0200

    [FE] Add visibility checkbox in UserToDoGroup

--- frontend/src/components/todo/UserToDoGroup.vue
+++ frontend/src/components/todo/UserToDoGroup.vue
@@ -5,15 +5,29 @@
     <button class="details-button" @click="navigateToDetails">
       {{ t("see-details") }}
     </button>
+
+    <div class="visibility-container" v-if="props.showChangeVisibleButton">
+      <p>Widoczność:</p>
+      <label class="theme-switch">
+        <input
+          type="checkbox"
+          v-model="props.todoGroup.visible"
+          @change="changeVisibilityGroup"
+        />
+        <span class="slider"></span>
+      </label>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useToDoGroup } from "@/composables/useToDoGroup";
 import { ToDoGroup } from "@/types";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const { changeGroupVisibility } = useToDoGroup();
 const { t } = useI18n();
 
 const navigateToDetails = () => {
@@ -25,8 +39,16 @@ const navigateToDetails = () => {
   }
 };
 
+const changeVisibilityGroup = async () => {
+  console.log(props.todoGroup.name);
+  console.log(props.todoGroup.visible);
+
+  await changeGroupVisibility(props.todoGroup.id);
+};
+
 const props = defineProps<{
   todoGroup: ToDoGroup;
+  showChangeVisibleButton: boolean;
 }>();
 </script>
 
@@ -47,6 +69,60 @@ const props = defineProps<{
   border: none;
   padding: 10px;
   cursor: pointer;
+}
+
+.visibility-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+
+/* Checkbox for visibility */
+.theme-switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 20px;
+}
+
+.theme-switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--color-border);
+  transition: 0.4s;
+  border-radius: 20px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 12px;
+  width: 12px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: rgb(66, 184, 131);
+}
+
+input:checked + .slider:before {
+  transform: translateX(20px);
 }
 
 @media (max-width: 768px) {
