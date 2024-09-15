5316c1e460be9ad0cd9f2c28c0458d5a74f01981
kacper0276 <kacper0276@op.pl>
2024-09-15 03:35:06 +0200

    [FE] Fix in authGuard

--- frontend/src/guards/authGuard.ts
+++ frontend/src/guards/authGuard.ts
@@ -1,12 +1,14 @@
 import { useAuthStore } from "@/stores/authStore";
 import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
-export const authGuard = (
+export const authGuard = async (
   _to: RouteLocationNormalized,
   _from: RouteLocationNormalized,
   next: NavigationGuardNext
 ) => {
   const authStore = useAuthStore();
+
+  await authStore.initializeAuth();
 
   if (authStore.loggedIn) {
     next();
