4cbec787334f518311090e5b76952a279bbea24f
kacper0276 <kacper0276@op.pl>
2024-09-15 03:36:49 +0200

    [FE] Create unauthGuard

---
+++ frontend/src/guards/unauthGuard.ts
@@ -0,0 +1,18 @@
+import { useAuthStore } from "@/stores/authStore";
+import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
+
+export const unauthGuard = async (
+  _to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const authStore = useAuthStore();
+
+  await authStore.initializeAuth();
+
+  if (authStore.loggedIn) {
+    next("/");
+  } else {
+    next();
+  }
+};
