57106dc6a28cc625113a9b2f32a140042cc99314
kacper0276 <kacper0276@op.pl>
2024-09-20 04:33:54 +0200

    [FE] Use new navigation in AdminPanelView

--- frontend/src/views/admin/AdminPanelView.vue
+++ frontend/src/views/admin/AdminPanelView.vue
@@ -1,14 +1,58 @@
 <script setup lang="ts">
+import ChangeAdminDataForm from "@/components/forms/ChangeAdminDataForm.vue";
+import AdminPanelNavigation from "@/components/nav/AdminPanelNavigation.vue";
 import { setDocumentTitle } from "@/composables/setDocumentTitle";
+import { markRaw, ref } from "vue";
 
 const { setTitle } = setDocumentTitle("admin-panel");
 setTitle("admin-panel");
+
+const componentsMap: Record<string, any> = {
+  ChangeAdminDataForm: ChangeAdminDataForm,
+};
+
+const currentPanel = ref<any>(null);
+
+const handlePanelChange = (panelName: string) => {
+  currentPanel.value = markRaw(componentsMap[panelName] || null);
+};
 </script>
 
 <template>
-  <div>
-    <h1>Admin panel</h1>
+  <div class="main-container">
+    <AdminPanelNavigation @panel-change="handlePanelChange" />
+    <div class="panel-view">
+      <component :is="currentPanel" />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-container {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  display: flex;
+  flex-direction: row;
+}
+
+.panel-view {
+  height: 100%;
+  width: 70%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+@media (max-width: 768px) {
+  .main-container {
+    flex-direction: column;
+  }
+
+  .panel-view {
+    width: 100%;
+    padding: 20px;
+  }
+}
+</style>
