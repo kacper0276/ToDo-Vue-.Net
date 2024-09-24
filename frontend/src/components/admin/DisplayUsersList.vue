b03635629eddd783ee10cdb7d4576e9a987e452a
kacper0276 <kacper0276@op.pl>
2024-09-24 05:49:55 +0200

    [FE] Create user data view

--- frontend/src/components/admin/DisplayUsersList.vue
+++ frontend/src/components/admin/DisplayUsersList.vue
@@ -1,10 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <UserData v-for="user in users" :key="user.id" :user="user"></UserData>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useUser } from "@/composables/useUser";
 import { onMounted } from "vue";
+import UserData from "@/components/admin/UserData.vue";
 
 const { users, getAllUsers } = useUser();
 
--- frontend/src/components/admin/UserData.vue
+++ frontend/src/components/admin/UserData.vue
@@ -1,7 +1,56 @@
-<script lang="ts" setup></script>
-
 <template>
-  <div></div>
+  <div class="user-card">
+    <div class="user-info">
+      <h2 class="user-login">{{ user.login }}</h2>
+      <p class="user-email">{{ user.email }}</p>
+      <p class="user-role">Role: {{ user.role }}</p>
+      <p class="user-groups">Number of groups: {{ user.toDoGroups.length }}</p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script lang="ts" setup>
+import type { User } from "@/types";
+
+defineProps<{ user: User }>();
+</script>
+
+<style scoped>
+.user-card {
+  background-color: var(--form-background);
+  color: var(--text-color);
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+  border: 1px solid var(--border-color);
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.user-login {
+  font-size: 1.5rem;
+  color: var(--primary-green);
+}
+
+.user-email {
+  font-size: 1rem;
+  color: var(--text-color);
+}
+
+.user-role,
+.user-groups {
+  font-size: 0.9rem;
+  color: var(--text-color);
+}
+
+.user-card:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  border-color: var(--button-hover);
+}
+</style>
