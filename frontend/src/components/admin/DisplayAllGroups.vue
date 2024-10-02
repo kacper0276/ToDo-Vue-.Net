aa7a69921a803e17f88228e1836570e072c70e9d
kacper0276 <kacper0276@op.pl>
2024-10-02 14:18:21 +0200

    [FE] Display all groups

--- frontend/src/components/admin/DisplayAllGroups.vue
+++ frontend/src/components/admin/DisplayAllGroups.vue
@@ -1,7 +1,39 @@
 <template>
-  <div></div>
+  <div class="main-container">
+    <UserToDoGroup
+      v-for="group of groups"
+      :show-change-visible-button="false"
+      :show-edit-button="true"
+      :todo-group="group"
+      @change-actual-group="handleGroup"
+    />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useToDoGroup } from "@/composables/useToDoGroup";
+import { onMounted, ref } from "vue";
+import UserToDoGroup from "../todoGroup/UserToDoGroup.vue";
+import type { ToDoGroup } from "@/types";
 
-<style scoped></style>
+const { groups, fetchToDoGroups } = useToDoGroup();
+const actualGroup = ref<ToDoGroup | null>(null);
+
+const handleGroup = (group: ToDoGroup) => {
+  actualGroup.value = group;
+};
+
+onMounted(async () => {
+  await fetchToDoGroups();
+});
+</script>
+
+<style scoped>
+.main-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
